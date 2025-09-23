56ff7394116f9e8e2403d58f5c365e887359dfb8
SaravananG <saravanang@syncfusion.com>
2025-09-23 15:17:15 +0530

    added the files

---
+++ Controllers/RichTextEditorController.cs
@@ -0,0 +1,175 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Cors;
+using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Http.Features;
+using System.Net;
+using System.Text.RegularExpressions;
+using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using Syncfusion.DocIORenderer;
+using Syncfusion.Pdf;
+using System.IO;
+using System.Reflection.Metadata;
+using Syncfusion.HtmlConverter;
+using System.Web;
+
+namespace RTEImageWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class RichTextEditorController : Controller
+    {
+
+        private int x = 0;
+        private string imageFile;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private Microsoft.AspNetCore.Hosting.IHostingEnvironment hostingEnv;
+        private int count = 1;
+        public RichTextEditorController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+       
+      
+        public class ExportParam
+        {
+            public string html { get; set; }
+        }
+        [AcceptVerbs("Post")]
+        [EnableCors("AllowAllOrigins")]
+        [Route("ExportToPdf")]
+        public ActionResult ExportToPdf([FromBody] ExportParam args)
+        {
+            string htmlString = args.html;
+            if (htmlString == null && htmlString == "")
+            {
+                return null;
+            }
+            using (WordDocument wordDocument = new WordDocument())
+            {
+                //This method adds a section and a paragraph in the document
+                wordDocument.EnsureMinimal();
+                wordDocument.HTMLImportSettings.ImageNodeVisited += OpenImage;
+                //Append the HTML string to the paragraph.
+                wordDocument.LastParagraph.AppendHTML(htmlString);
+                DocIORenderer render = new DocIORenderer();
+                //Converts Word document into PDF document
+                PdfDocument pdfDocument = render.ConvertToPDF(wordDocument);
+                wordDocument.HTMLImportSettings.ImageNodeVisited -= OpenImage;
+                MemoryStream stream = new MemoryStream();
+                pdfDocument.Save(stream);
+                return File(stream.ToArray(), System.Net.Mime.MediaTypeNames.Application.Pdf, "Sample.pdf");
+            }
+        }
+
+
+        [AcceptVerbs("Post")]
+        [EnableCors("AllowAllOrigins")]
+        [Route("ExportToDocs")]
+        public FileStreamResult ExportToDocs([FromBody] ExportParam args)
+        {
+            string htmlString = args.html;
+            if (htmlString == null && htmlString == "")
+            {
+                return null;
+            }
+            using (WordDocument document = new WordDocument())
+            {
+                document.EnsureMinimal();
+                //Hooks the ImageNodeVisited event to open the image from a specific location
+                document.HTMLImportSettings.ImageNodeVisited += OpenImage;
+                //Validates the Html string
+                bool isValidHtml = document.LastSection.Body.IsValidXHTML(htmlString, XHTMLValidationType.None);
+                //When the Html string passes validation, it is inserted to the document
+                if (isValidHtml)
+                {
+                    //Appends the Html string to first paragraph in the document
+                    document.Sections[0].Body.Paragraphs[0].AppendHTML(htmlString);
+                }
+                //Unhooks the ImageNodeVisited event after loading HTML
+                document.HTMLImportSettings.ImageNodeVisited -= OpenImage;
+                //Creates file stream.
+                MemoryStream stream = new MemoryStream();
+                document.Save(stream, FormatType.Docx);
+                stream.Position = 0;
+                //Download Word document in the browser
+                return File(stream, "application/msword", "Result.docx");
+            }
+        }
+
+        
+
+          
+       
+        private string SanitizeHtml(string html)
+        {
+            // Remove or replace non-ASCII or control characters
+            // For example, you can use regular expressions to replace them with spaces
+            // Regex pattern to match non-ASCII or control characters: [^\x20-\x7E]
+            return Regex.Replace(html, @"[^\x20-\x7E]", " ");
+        }
+
+        [AcceptVerbs("Post")]
+        [EnableCors("AllowAllOrigins")]
+        [Route("ImportFromDocs")]
+        public IActionResult ImportFromDocs(IList<IFormFile> UploadFiles)
+        {
+            string HtmlString = string.Empty;
+            if (UploadFiles != null)
+            {
+                foreach (var file in UploadFiles)
+                {
+                    string filename = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                    filename = _webHostEnvironment.WebRootPath + $@"\{filename}";
+                    using (FileStream fs = System.IO.File.Create(filename))
+                    {
+                        file.CopyTo(fs);
+                        fs.Flush();
+                    }
+                    using (var mStream = new MemoryStream())
+                    {
+                        WordDocument document = new WordDocument(file.OpenReadStream(), FormatType.Rtf);
+                        document.SaveOptions.HTMLExportWithWordCompatibility = false;
+                        document.Save(mStream, FormatType.Html);
+                        mStream.Position = 0;
+                        HtmlString = new StreamReader(mStream).ReadToEnd();
+                    }
+                    ;
+                    HtmlString = ExtractBodyContent(HtmlString);
+                    HtmlString = SanitizeHtml(HtmlString);
+                    System.IO.File.Delete(filename);
+                }
+                return Ok(HtmlString);
+            }
+            else
+            {
+                Response.Clear();
+                return BadRequest("No files were uploaded.");
+            }
+        }
+      
+
+        private static void OpenImage(object sender, ImageNodeVisitedEventArgs args)
+        {
+            if (args.Uri.StartsWith("https://"))
+            {
+                WebClient client = new WebClient();
+                //Download the image as a stream.
+                byte[] image = client.DownloadData(args.Uri);
+                Stream stream = new MemoryStream(image);
+                //Set the retrieved image from the input Markdown.
+                args.ImageStream = stream;
+            }
+        }
+
+        private string ExtractBodyContent(string html)
+        {
+            if (html.Contains("<html") && html.Contains("<body"))
+            {
+                return html.Remove(0, html.IndexOf("<body>") + 6).Replace("</body></html>", "");
+            }
+            return html;
+        }
+
+       
+    }
+}
